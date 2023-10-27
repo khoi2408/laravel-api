dc97ad0f3870d3987a09334be8241090d5eb310a
khoi le <lmkhoi2k@gmail.com>
2023-10-27 12:29:12 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// api/v1
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    // api/v1/customers
+    Route::apiResource('customers', CustomerController::class);
+    // api/v1/invoices
+    Route::apiResource('invoices', InvoiceController::class);
+});
